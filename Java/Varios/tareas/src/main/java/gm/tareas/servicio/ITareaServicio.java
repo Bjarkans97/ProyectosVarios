efd52508758c9fdc1c02aa66dec5df6bba2fb598
Bjarkans <benjamin.nramirezs@gmail.com>
2025-10-09 20:24:38 -0300

    Implementacion de tarea servicio

--- Java/Varios/tareas/src/main/java/gm/tareas/servicio/ITareaServicio.java
+++ Java/Varios/tareas/src/main/java/gm/tareas/servicio/ITareaServicio.java
@@ -6,4 +6,10 @@ import java.util.List;
 
 public interface ITareaServicio {
     public List<Tarea> listarTareas();
+
+    public Tarea buscarTareaPorId(Integer id);
+
+    public void guardarTarea(Tarea tarea);
+
+    public void eliminarTarea(Tarea tarea);
 }
