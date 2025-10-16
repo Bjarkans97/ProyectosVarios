c2230414efc6220056bea029bb02e489a27ed90e
Bjarkans <benjamin.nramirezs@gmail.com>
2025-10-16 00:58:52 -0300

    Version corregida

---
+++ Java/Angular/inventarios-new-app/src/app/producto-lista/producto-lista.ts
@@ -0,0 +1,31 @@
+import { Component, inject } from '@angular/core';
+import { Producto } from '../producto';
+import { ProductoService } from '../producto.service';
+
+@Component({
+  selector: 'app-producto-lista',
+  imports: [],
+  templateUrl: './producto-lista.html',
+  styleUrl: './producto-lista.css'
+})
+export class ProductoLista {
+  productos!: Producto[];
+
+  private productoServicio = inject(ProductoService);
+
+  ngOnInit(){
+    this.obtenerProductos();
+  }
+
+  private obtenerProductos(): void {
+    this.productoServicio.obtenerProductosLista().subscribe({
+      next: (datos) => {
+        this.productos = datos;
+      },
+      error: (error) => {
+        console.error("Error al obtener productos: ", error);
+      }
+    });
+  }
+
+}
